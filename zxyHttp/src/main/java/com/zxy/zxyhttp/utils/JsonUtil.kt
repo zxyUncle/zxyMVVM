75b69d5672d14326c352c876c119195d7552cefa
张三疯 <659966631@qq.com>
2021-01-04 16:33:07 +0800

    update

---
+++ zxyHttp/src/main/java/com/zxy/zxyhttp/utils/JsonUtil.kt
@@ -0,0 +1,29 @@
+package com.zxy.zxyhttp.utils
+
+import com.google.gson.Gson
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+
+/**
+ * Created by zxy on 2020/4/2 0002 19:57
+ * ******************************************
+ * *  将键值对转换成 RequestBody
+ * ******************************************
+ */
+
+object JsonUtil {
+    fun bodyVararg(vararg args: String): RequestBody {
+        val map = HashMap<String, String>()
+        for (i in args.indices step 2) {
+            map[args[i]] = args[i + 1]
+        }
+        val json = Gson().toJson(map)
+        return RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), json)
+    }
+    fun bodyMap(map:Map<String, Any>): RequestBody {
+        val json = Gson().toJson(map)
+        return RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), json)
+    }
+
+}
